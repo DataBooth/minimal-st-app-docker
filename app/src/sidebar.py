74f7bc2b9de6c3df7a6cfb27f0d8e580507a901d
Michael Booth <michael@databooth.com.au>
2022-08-24 23:24:32 +1000

    progress

--- app/src/sidebar.py
+++ app/src/sidebar.py
@@ -1,7 +1,14 @@
+from urllib.parse import urlparse
 import streamlit as st
+
 
 def create_sidebar_main():
     st.sidebar.header("Sidebar Main Header")
+
+    # DOESN'T WORK
+    # frontend_url = request.META.get('HTTP_REFERER')
+    # url = urlparse(frontend_url)
+    # st.sidebar.write(url)
 
 
 def create_sidebar_utilities():
