7724e37b002626d93252fa4dac264604da83a6a9
Michael Booth <michael@databooth.com.au>
2022-08-26 15:51:56 +1000

    sort of working linked multiselect

--- app/src/sidebar.py
+++ app/src/sidebar.py
@@ -3,40 +3,37 @@ import streamlit as st
 
 
 def list_diff(list1, list2):
-    return list(set(list1).symmetric_difference(set(list2)))
+    return sorted(list(set(list1).symmetric_difference(set(list2))))
 
 
 def create_sidebar_main(df: pd.DataFrame, target="Survived"):
     st.sidebar.subheader("Column (Feature) selection:")
 
-    feat_all = list(df.columns)
+    feat_all = sorted(list(df.columns))
     feat_all.remove(target)    # remove target variable from features
-
-    try:
-        st.code(st.session_state.feat_select)
-        st.code(st.session_state.feat_discard)
-    except:
-        pass
 
     # Initialise state variables for selected & discard features
 
     if "feat_select" not in st.session_state:
         feat_select = feat_all
-        st.session_state.feat_select = feat_select
+        st.session_state.feat_select = sorted(feat_select)
 
     if "feat_discard" not in st.session_state:
         feat_discard = None
         st.session_state.feat_discard = feat_discard
 
     st.session_state.feat_select = st.sidebar.multiselect(label="Include columns (feat):",
-            options=st.session_state.feat_select, default=st.session_state.feat_select)
+            options=st.session_state.feat_select, default=st.session_state.feat_select, key=1)
 
     st.session_state.feat_discard = list_diff(feat_all, st.session_state.feat_select)
     
     st.session_state.feat_discard = st.sidebar.multiselect(label="Discard columns (feat):",
-            options=st.session_state.feat_discard, default=st.session_state.feat_discard)
+            options=st.session_state.feat_discard, default=st.session_state.feat_discard, key=2)
 
     st.session_state.feat_select = list_diff(feat_all, st.session_state.feat_discard)
+
+    st.write(st.session_state.feat_select)
+    st.write(st.session_state.feat_discard)
 
 
 def create_sidebar_utilities():
